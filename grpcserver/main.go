0497c0c2013c5cc88eaecb8b05e7eedd79d48105
João Pedro <joao.souza@vocedm.com.br>
2023-12-27 10:32:48 -0300

    add

--- grpcserver/main.go
+++ grpcserver/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"flag"
 	pb "github.com/JP-93/grpc-example/protobuf/hello/v1"
-	"github.com/playground.com/grpcserver/health"
 	"github.com/playground.com/grpcserver/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"log"
 	"net"
+	"time"
+)
+
+var (
+	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
+
+	system = "" // empty string represents the health of the system
 )
 
 func main() {
@@ -18,19 +26,33 @@ func main() {
 	}
 	log.Println("running")
 
-	newCheck := health.NewHealth()
-	s := &service.Service{
-		Hc: newCheck,
-	}
+	s := &service.Service{}
 
 	grpcServer := grpc.NewServer()
+	healthcheck := health.NewServer()
 
 	pb.RegisterHelloServiceServer(grpcServer, s)
-	healthpb.RegisterHealthServer(grpcServer, s)
+	healthpb.RegisterHealthServer(grpcServer, healthcheck)
 
-	err = grpcServer.Serve(listener)
-	if err != nil {
-		log.Fatalf("error serving new tcp listener, %v", err)
+	go func() {
+		// asynchronously inspect dependencies and toggle serving status as needed
+		next := healthpb.HealthCheckResponse_SERVING
+
+		for {
+			healthcheck.SetServingStatus(system, next)
+
+			if next == healthpb.HealthCheckResponse_SERVING {
+				next = healthpb.HealthCheckResponse_NOT_SERVING
+			} else {
+				next = healthpb.HealthCheckResponse_SERVING
+			}
+
+			time.Sleep(*sleep)
+		}
+	}()
+
+	if err = grpcServer.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 
 }
