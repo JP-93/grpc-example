3326b60511df7d0ca2b3f61e0a875f66d1c0f25c
João Pedro <joao.souza@vocedm.com.br>
2023-12-27 10:46:58 -0300

    remove

--- grpcserver/service/service.go
+++ grpcserver/service/service.go
@@ -4,53 +4,12 @@ import (
 	"context"
 	"fmt"
 	pb "github.com/JP-93/grpc-example/protobuf/hello/v1"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"log"
 )
 
 type Service struct {
 	pb.UnimplementedHelloServiceServer
-	healthpb.UnimplementedHealthServer
-	//Hc health.Health
 }
-
-//var (
-//	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
-//)
-//
-//func (s *Service) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
-//	ht := ghc.NewServer()
-//	var res *healthpb.HealthCheckResponse
-//
-//	go func() {
-//		res.Status = healthpb.HealthCheckResponse_SERVING
-//		for {
-//			ht.SetServingStatus("", res.Status)
-//
-//			if res.Status == healthpb.HealthCheckResponse_SERVING {
-//				res.Status = healthpb.HealthCheckResponse_NOT_SERVING
-//			} else {
-//				res.Status = healthpb.HealthCheckResponse_SERVING
-//			}
-//
-//			time.Sleep(*sleep)
-//		}
-//	}()
-//
-//	_, err := s.Hc.Check(ctx, res)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return res, nil
-//}
-//
-//func (s *Service) Watch(request *healthpb.HealthCheckRequest, server healthpb.Health_WatchServer) error {
-//	return server.Send(&healthpb.HealthCheckResponse{
-//		Status: healthpb.HealthCheckResponse_SERVING,
-//	})
-//}
 
 func (s *Service) CreateHello(ctx context.Context, hello *pb.Hello) (*pb.HelloResponse, error) {
 	conc := fmt.Sprintf("%s %s", hello.Nome, hello.Msg)
